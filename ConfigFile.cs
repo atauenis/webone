ae15b3d4ce50b10de03e1b95f5ed19f6ec26233e
Alexander Tauenis <atauenis@yandex.ru>
2019-05-25 00:01:28 +0300

    add some config file parsing

--- ConfigFile.cs
+++ ConfigFile.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace WebOne
 {
@@ -11,8 +12,8 @@ namespace WebOne
 	/// </summary>
 	static class ConfigFile
 	{
-		//тут будут все ключи из конфиг-файла программы. Но пока файла нет, все они жёстко захардкожены здесь.
 		static string ConfigFileName = "/dev/ceiling"; //с потолка
+		static string[] SpecialSections = { "ForceHttps", "TextTypes", "UA:", "URL:" }; //like "UA:Mozilla/3.*"
 
 		/// <summary>
 		/// TCP port that should be used by the Proxy Server
@@ -46,7 +47,57 @@ namespace WebOne
 		public static Encoding OutputEncoding = Encoding.Default;
 
 		static ConfigFile() {
+			ConfigFileName = "webone.conf";
 			Console.WriteLine("Using configuration file {0}.", ConfigFileName);
+
+			if (!File.Exists(ConfigFileName)) return;
+			string[] CfgFile = System.IO.File.ReadAllLines(ConfigFileName);
+			string Section = "";
+			for(int i = 0; i<CfgFile.Count(); i++) {
+				if (CfgFile[i] == "") continue; //empty lines
+				if (CfgFile[i].StartsWith(";")) continue; //comments
+				if (CfgFile[i].StartsWith("[")) //section
+				{
+					Section = CfgFile[i].Substring(1, CfgFile[i].Length - 2);
+					continue;
+				}
+				if(i > 1 && CfgFile[i] == "" && CfgFile[i-1] == "") //section separator
+				{
+					Section = "";
+					continue;
+				}
+
+				/*Console.WriteLine(Section);
+				if(Program.CheckString(Section, SpecialSections)) //special sections (patterns, lists, etc)
+				{
+					Console.WriteLine("Special: " + Section);
+					continue;
+				}*/
+				
+				int BeginValue = CfgFile[i].IndexOf("=");//regular sections
+				if (BeginValue == 0) continue; //bad line
+				string ParamName = CfgFile[i].Substring(0, BeginValue);
+				string ParamValue = CfgFile[i].Substring(BeginValue + 1);
+				//Console.WriteLine("{0}.{1}={2}", Section, ParamName, ParamValue);
+
+				switch (Section) {
+					case "Server":
+						switch(ParamName) {
+							case "Port":
+								Port = Convert.ToInt32(ParamValue);
+								break;
+							default:
+								Console.WriteLine("Unknown server option: " + ParamName);
+								break;
+						}
+						break;
+					default:
+						Console.WriteLine("Unknown section: " + ParamName);
+						break;
+				}
+
+			}
+			Console.WriteLine("{0} load complete.", ConfigFileName);
 		}
 	}
 }
