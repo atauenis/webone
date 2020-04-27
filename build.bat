d0fffa94b83032580041ac5fbf4998a2cb7be2d4
Alexander Tauenis <atauenis@yandex.ru>
2020-04-27 15:28:56 +0300

    add support for zip/deb/prm package building

---
+++ build.bat
@@ -0,0 +1,16 @@
+@rem Requires https://github.com/qmfrederik/dotnet-packaging/
+@echo Building WebOne for Debian/Ubuntu, RedHat/CentOS and Windows...
+rmdir bin\Release /S
+dotnet restore
+dotnet publish -r linux-x64 -c Release --self-contained false
+dotnet deb --no-restore -c Release -r linux-x64
+dotnet rpm --no-restore -c Release -r linux-x64
+dotnet publish -r linux-arm -c Release --self-contained false
+dotnet deb --no-restore -c Release -r linux-arm
+dotnet rpm --no-restore -c Release -r linux-arm
+@rem dotnet publish -r osx-x64 -c Release --self-contained false
+@rem dotnet pkg -r osx-x64 -c Release
+dotnet publish -r win-x86 -c Release --self-contained false
+dotnet zip --no-restore -c Release -r win-x86
+@rem Win32 build must be last because else VS debugging will be broken.
+@echo All platforms and kinds of packages are processed.
