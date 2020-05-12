f2b3035d0721f1599ff646a1b2f150d7d33c7cf8
Alexander Tauenis <atauenis@yandex.ru>
2020-05-12 18:34:33 +0300

    add win-x64 build

--- build.bat
+++ build.bat
@@ -1,17 +1,20 @@
 @rem Requires https://github.com/qmfrederik/dotnet-packaging/
-@echo Building WebOne for Debian/Ubuntu, RedHat/CentOS and Windows...
+@echo Building WebOne for Debian/Ubuntu, RedHat/CentOS, MacOSX and Windows...
 rmdir bin\Release /S
 rmdir bin\ReleaseWin32 /S
 dotnet restore
-dotnet publish -r linux-x64 -c Release --self-contained false
+dotnet publish -c Release -r linux-x64 --self-contained false
 dotnet deb --no-restore -c Release -r linux-x64
 dotnet rpm --no-restore -c Release -r linux-x64
-dotnet publish -r linux-arm -c Release --self-contained false
+dotnet publish -c Release -r linux-arm --self-contained false
 dotnet deb --no-restore -c Release -r linux-arm
 dotnet rpm --no-restore -c Release -r linux-arm
-@rem dotnet publish -r osx-x64 -c Release --self-contained false
+dotnet publish -c Release -r osx-x64 --self-contained false
 @rem dotnet pkg -r osx-x64 -c Release
-dotnet publish -r win-x86 -c ReleaseWin32 --self-contained false
+dotnet zip --no-restore -c Release -r osx-x64
+dotnet publish -c ReleaseWin32 -r win-x86 --self-contained false
 dotnet zip --no-restore -c ReleaseWin32 -r win-x86
+dotnet publish -c ReleaseWin32 -r win-x64 --self-contained false
+dotnet zip --no-restore -c ReleaseWin32 -r win-x64
 @rem Win32 build must be last because else VS debugging will be broken.
 @echo All platforms and kinds of packages are processed.
