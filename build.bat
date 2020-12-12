2086797729740995a769b8c50ea73421a1e935d3
Alexander Tauenis <atauenis@yandex.ru>
2020-12-12 19:57:23 +0300

    update build script again; update config file

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 @rem Requires https://github.com/qmfrederik/dotnet-packaging/
+@rem Remember to run `dotnet tool update --global dotnet-rpm/deb/zip/tarball`
 @echo Building WebOne for Debian/Ubuntu, RedHat/CentOS, macOS and Windows...
 rmdir bin\Release /S
 rmdir bin\ReleaseSC /S
@@ -6,12 +7,12 @@ dotnet restore
 dotnet publish -c Release -r linux-x64
 dotnet deb --no-restore -c Release -r linux-x64
 dotnet rpm --no-restore -c Release -r linux-x64
-dotnet publish -c Release -r linux-arm
-dotnet deb --no-restore -c Release -r linux-arm
-dotnet rpm --no-restore -c Release -r linux-arm
+dotnet publish -c ReleaseSC -r linux-arm
+dotnet deb --no-restore -c ReleaseSC -r linux-arm
+dotnet rpm --no-restore -c ReleaseSC -r linux-arm
 dotnet publish -c Release -r osx-x64
 @rem dotnet pkg -r osx-x64 -c Release
-dotnet zip --no-restore -c Release -r osx-x64
+dotnet tarball --no-restore -c Release -r osx-x64
 dotnet publish -c Release -r win-x86
 dotnet zip --no-restore -c Release -r win-x86
 dotnet zip --no-restore -c ReleaseSC -r win-x86
