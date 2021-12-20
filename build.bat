b006db35df61e4ca91fad0cada787655d3e71581
Александр Тауенис <atauenis@yandex.ru>
2021-12-20 14:15:44 +0300

    add dotnet-packaging autoupdate in build script

--- build.bat
+++ build.bat
@@ -1,8 +1,25 @@
-@rem Requires https://github.com/qmfrederik/dotnet-packaging/
-@rem Remember to run `dotnet tool update --global dotnet-rpm/deb/zip/tarball`
+@echo WebOne build script	20.12.2021
+@echo.
+
+@echo Clean up directories:
+@rmdir bin\Release /S
+@rmdir bin\ReleaseSC /S
+@echo.
+
+@echo Checking for packaging toolkit updates...
+@dotnet tool update --global dotnet-zip
+@IF ERRORLEVEL 1 GOTO NoToolkit
+@IF ERRORLEVEL 9009 GOTO NoNetSDK
+@dotnet tool update --global dotnet-deb
+@IF ERRORLEVEL 1 GOTO NoToolkit
+@dotnet tool update --global dotnet-rpm
+@IF ERRORLEVEL 1 GOTO NoToolkit
+@dotnet tool update --global dotnet-tarball
+@IF ERRORLEVEL 1 GOTO NoToolkit
+@echo.
+
+:Build
 @echo Building WebOne for Debian/Ubuntu, RedHat/CentOS, macOS and Windows...
-rmdir bin\Release /S
-rmdir bin\ReleaseSC /S
 dotnet restore
 dotnet publish -c Release -r linux-x64
 dotnet deb --no-restore -c Release -r linux-x64
@@ -10,6 +27,9 @@ dotnet rpm --no-restore -c Release -r linux-x64
 dotnet publish -c ReleaseSC -r linux-arm
 dotnet deb --no-restore -c ReleaseSC -r linux-arm
 dotnet rpm --no-restore -c ReleaseSC -r linux-arm
+@rem dotnet publish -c ReleaseSC -r linux-arm64
+@rem dotnet deb --no-restore -c ReleaseSC -r linux-arm64
+@rem dotnet rpm --no-restore -c ReleaseSC -r linux-arm64
 dotnet publish -c Release -r osx-x64
 @rem dotnet pkg -r osx-x64 -c Release
 dotnet tarball --no-restore -c Release -r osx-x64
@@ -21,4 +41,31 @@ dotnet zip --no-restore -c Release -r win-arm
 dotnet publish -c Release -r win-x64
 dotnet zip --no-restore -c Release -r win-x64
 @rem Win32 build must be last because else VS debugging will be broken.
+@echo.
 @echo All platforms and kinds of packages are processed.
+@echo.
+@GOTO :EOF
+
+:NoToolkit
+@echo BUILD PROBLEM:
+@echo Your environment does not have Packaging utilities for .NET Core installed.
+@echo Press any key to install them and try again, or Ctrl+C to exit this script now.
+@echo.
+@pause
+dotnet tool install --global dotnet-zip
+dotnet tool install --global dotnet-tarball
+dotnet tool install --global dotnet-rpm
+dotnet tool install --global dotnet-deb
+dotnet zip install
+dotnet tarball install
+dotnet rpm install
+dotnet deb install
+@IF ERRORLEVEL 0 GOTO Build
+@echo Seems, an error occured. How about to read this: https://github.com/quamotion/dotnet-packaging ?
+@GOTO :EOF
+
+:NoNetSDK
+@echo ERROR:
+@echo You need to have Microsoft .NET Core SDK 3.1 or Microsoft Visual Studio 2019+.
+@echo It can be downloaded here: https://dotnet.microsoft.com/en-us/download/dotnet/3.1
+@echo                   or here: https://visualstudio.microsoft.com/ru/vs/community/
