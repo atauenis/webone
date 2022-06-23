07b3cc904ef202675742887a6efb72322ed01045
Александр Тауенис <atauenis@yandex.ru>
2022-06-23 20:06:34 +0300

    add youtube-dl support under Linux (yt.sh script)

---
+++ LinuxAndMacOS/yt.sh
@@ -0,0 +1,4 @@
+﻿#!/bin/sh
+#Download a YouTube video and return it as a stream of MTS with MPEG2 data.
+#Requires youtube-dl and ffmpeg
+youtube-dl "$1" -o - | ffmpeg -i pipe: -vcodec mpeg2video -acodec mp2 -f mpegts pipe:
